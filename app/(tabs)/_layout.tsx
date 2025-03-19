c2bb1ac6cb4024b91dbe91ac675df60e2d91bd8f
Neal Uehara <ueharaneal@gmail.com>
2025-03-18 23:18:57 -0700

    cleared files

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,38 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import { Tabs } from "expo-router"
+import React from "react"
+import { Platform } from "react-native"
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { Colors } from "@/constants/Colors"
+import Ionicons from "@expo/vector-icons/build/Ionicons"
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+	return (
+		<Tabs
+			screenOptions={{
+				tabBarActiveTintColor: "#0a7ea4",
+				headerShown: false,
+				tabBarStyle: Platform.select({
+					ios: {
+						// Use a transparent background on iOS to show the blur effect
+						position: "absolute",
+					},
+					default: {},
+				}),
+			}}
+		>
+			<Tabs.Screen
+				name='index'
+				options={{
+					title: "Home",
+					tabBarIcon: ({ color, focused }) => (
+						<Ionicons
+							size={28}
+							name={focused ? "home" : "home-outline"}
+							color={color}
+						/>
+					),
+				}}
+			/>
+		</Tabs>
+	)
 }
