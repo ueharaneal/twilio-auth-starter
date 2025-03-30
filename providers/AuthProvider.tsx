5bea0de615f2cdc16bc90a89d1af5e8044e30b31
Neal Uehara <ueharaneal@gmail.com>
2025-03-29 22:38:16 -0700

    fixed sign out button

--- providers/AuthProvider.tsx
+++ providers/AuthProvider.tsx
@@ -2,10 +2,12 @@ import { View, Text } from "react-native"
 import React, { useState, useEffect } from "react"
 import { Session } from "@supabase/supabase-js"
 import { supabase } from "@/lib/supabase"
+import { useRouter } from "expo-router"
 
 export const AuthContext = React.createContext({
 	user: {},
 	setUser: ({}) => {},
+	logOut: () => {},
 })
 
 export const useAuth = () => React.useContext(AuthContext)
@@ -17,19 +19,41 @@ export default function AuthProvider({
 }) {
 	const [user, setUser] = useState({})
 	const [session, setSession] = useState<Session | null>(null)
+
+	const router = useRouter()
+
+	const getUser = async (session: Session | null) => {
+		if (session) {
+			router.push("/(tabs)")
+			// const {
+			// 	data: { session },
+			// } = await supabase.auth.getSession()
+			// //when were ready get the actual user from the database
+			// return session?.user
+		}
+		return
+	}
+
+	const logOut = async () => {
+		await supabase.auth.signOut()
+		router.push("/(auth)")
+	}
+
 	useEffect(() => {
 		console.log("use effect called ")
 		supabase.auth.getSession().then(({ data: { session } }) => {
 			console.log(session, ": session")
 			setSession(session)
+			getUser(session)
 		})
 		supabase.auth.onAuthStateChange((_event, session) => {
 			console.log("authStatechange", session)
 			setSession(session)
+			getUser(session)
 		})
 	}, [])
 	return (
-		<AuthContext.Provider value={{ user, setUser }}>
+		<AuthContext.Provider value={{ user, setUser, logOut }}>
 			{children}
 		</AuthContext.Provider>
 	)
--- screens/settings/index.tsx
+++ screens/settings/index.tsx
@@ -1,13 +1,18 @@
 import React, { Component } from "react"
 import { Text, View } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
+import { Button, ButtonText } from "@/components/ui/button"
+import { useAuth } from "@/providers/AuthProvider"
 
 export default function Settings() {
+	const { logOut } = useAuth()
 	return (
 		<SafeAreaView>
 			<View>
 				<Text>Settings</Text>
-				<Text>View page </Text>
+				<Button onPress={logOut}>
+					<ButtonText>Log out</ButtonText>
+				</Button>
 			</View>
 		</SafeAreaView>
 	)
