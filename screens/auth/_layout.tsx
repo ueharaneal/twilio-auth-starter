b1674579a0194d0ea94fa78dab91064f319d38e0
Neal Uehara <ueharaneal@gmail.com>
2025-03-31 09:24:06 -0700

    layout changes

--- screens/auth/_layout.tsx
+++ screens/auth/_layout.tsx
@@ -1,4 +1,11 @@
-import { View, Text } from "react-native"
+import {
+	View,
+	Text,
+	KeyboardAvoidingView,
+	TouchableWithoutFeedback,
+	Platform,
+	Keyboard,
+} from "react-native"
 import React from "react"
 import { SafeAreaView } from "react-native"
 import ThreadsIcon from "@/assets/svg/ThreadsIcon"
@@ -16,16 +23,26 @@ export default ({
 }) => {
 	return (
 		<SafeAreaView>
-			<VStack className='items-center  gap-y-3'>
-				<ThreadsIcon size={50} />
-				<Text className='text-2xl font-bold'>Twilio Auth App</Text>
-			</VStack>
-			{children}
-			<Button className='items-center' onPress={onPress}>
-				<ButtonText className='text-2xl font-bold'>
-					{buttonName}{" "}
-				</ButtonText>
-			</Button>
+			<KeyboardAvoidingView
+				behavior={Platform.OS === "ios" ? "padding" : "height"}
+			>
+				<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+					<View className='justify-between h-full w-full '>
+						<VStack className='items-center justify-center'>
+							<ThreadsIcon size={40} />
+							<Text className='text-2xl font-bold'>Twilio Auth App</Text>
+						</VStack>
+						{children}
+						<VStack>
+							<Button className='items-center' onPress={onPress}>
+								<ButtonText className='text-2xl font-bold'>
+									{buttonName}{" "}
+								</ButtonText>
+							</Button>
+						</VStack>
+					</View>
+				</TouchableWithoutFeedback>
+			</KeyboardAvoidingView>
 		</SafeAreaView>
 	)
 }
